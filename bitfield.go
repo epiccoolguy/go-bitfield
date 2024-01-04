66facbc5a6760a9b3d1664c70909c655f1b0b023
Miguel Lo-A-Foe <miguel@loafoe.dev>
2024-01-04 13:20:33 +0100

    implement SetBit, ClearBit, ToggleBit and GetBit
    Signed-off-by: Miguel Lo-A-Foe <miguel@loafoe.dev>

--- bitfield.go
+++ bitfield.go
@@ -2,7 +2,6 @@ package bitfield
 
 import (
 	"errors"
-	"fmt"
 )
 
 // BitField represents a field of bits and provides methods for manipulating bits.
@@ -21,4 +20,72 @@ func New(n uint) *BitField {
 		bits: make([]byte, size),
 		sz:   n,
 	}
+}
+
+// calculateBitPosition calculates and returns the byte index and bit position within the byte
+// for a specified bit position in the BitField.
+// Parameters:
+// - pos uint: The position of the bit, in bits (0-based index).
+// Returns the byte index (int), the bit position within that byte (uint), and an error if the
+// given position is out of the range of the BitField (i.e., if it exceeds the size of the BitField).
+func (bf *BitField) calculateBitPosition(pos uint) (byteIndex int, bitPosition uint, err error) {
+	if pos >= bf.sz {
+		return 0, 0, errors.New("bit position out of range")
+	}
+	byteIndex = int(pos / 8) // Calculate the byte index.
+	bitPosition = pos % 8    // Calculate the bit position within the byte.
+	return byteIndex, bitPosition, nil
+}
+
+// SetBit sets a bit at a specified position within the BitField to 1.
+// Parameters:
+// - pos uint: The position of the bit to set, in bits (0-based index).
+// Returns an error if the position is out of the range of the BitField.
+func (bf *BitField) SetBit(pos uint) error {
+	byteIndex, bitPosition, err := bf.calculateBitPosition(pos)
+	if err != nil {
+		return err
+	}
+	bf.bits[byteIndex] |= (1 << bitPosition) // Set the bit.
+	return nil
+}
+
+// ClearBit clears a bit at a specified position within the BitField to 0.
+// Parameters:
+// - pos uint: The position of the bit to clear, in bits (0-based index).
+// Returns an error if the position is out of the range of the BitField.
+func (bf *BitField) ClearBit(pos uint) error {
+	byteIndex, bitPosition, err := bf.calculateBitPosition(pos)
+	if err != nil {
+		return err
+	}
+	bf.bits[byteIndex] &= ^(1 << bitPosition) // Clear the bit.
+	return nil
+}
+
+// ToggleBit toggles a bit at a specified position within the BitField.
+// Parameters:
+// - pos uint: The position of the bit to toggle, in bits (0-based index).
+// Returns an error if the position is out of the range of the BitField.
+func (bf *BitField) ToggleBit(pos uint) error {
+	byteIndex, bitPosition, err := bf.calculateBitPosition(pos)
+	if err != nil {
+		return err
+	}
+	bf.bits[byteIndex] ^= (1 << bitPosition) // Toggle the bit.
+	return nil
+}
+
+// TestBit retrieves the value of a bit at a specified position within the BitField.
+// Parameters:
+// - pos uint: The position of the bit to retrieve, in bits (0-based index).
+// Returns the value of the bit (true for 1, false for 0) and an error if the position is out of range.
+func (bf *BitField) TestBit(pos uint) (bool, error) {
+	byteIndex, bitPosition, err := bf.calculateBitPosition(pos)
+	if err != nil {
+		return false, err
+	}
+
+	// Retrieve the value of the bit by checking if the bit at the bitPosition is set in the byte.
+	return bf.bits[byteIndex]&(1<<bitPosition) != 0, nil
 }
--- bitfield_test.go
+++ bitfield_test.go
@@ -5,14 +5,46 @@ import (
 	"testing"
 )
 
-// Test case struct for creating new BitFields
+// Test case structs
 type NewTestCase struct {
 	name        string // Name of the test case
 	n           uint   // Input size in bits for the New function
 	expectedLen int    // Expected length of the underlying byte slice
 }
 
-// Test cases for creating new BitFields
+type SetBitTestCase struct {
+	name         string    // Name of the test case
+	bf           *BitField // Initial BitField for the test
+	pos          uint      // Position of the bit to set
+	expectError  bool      // Whether an error is expected
+	expectedBits []byte    // Expected byte slice after setting the bit
+}
+
+type ClearBitTestCase struct {
+	name         string    // Name of the test case
+	bf           *BitField // Initial BitField for the test
+	pos          uint      // Position of the bit to clear
+	expectError  bool      // Whether an error is expected
+	expectedBits []byte    // Expected byte slice after clearing the bit
+}
+
+type ToggleBitTestCase struct {
+	name         string    // Name of the test case
+	bf           *BitField // Initial BitField for the test
+	pos          uint      // Position of the bit to toggle
+	expectError  bool      // Whether an error is expected
+	expectedBits []byte    // Expected byte slice after toggling the bit
+}
+
+type TestBitTestCase struct {
+	name          string    // Name of the test case
+	bf            *BitField // Initial BitField for the test
+	pos           uint      // Position of the bit to get
+	expectError   bool      // Whether an error is expected
+	expectedValue bool      // Expected value of the bit
+}
+
+// Test cases
 var newTestCases = []NewTestCase{
 	{
 		name:        "Zero size",
@@ -36,7 +68,126 @@ var newTestCases = []NewTestCase{
 	},
 }
 
-// TestNew tests the New function of the bitfield package.
+var setBitTestCases = []SetBitTestCase{
+	{
+		name: "Set first bit to true in 2-byte field",
+		bf: &BitField{
+			bits: []byte{0b00000000, 0b00000000},
+			sz:   16,
+		},
+		pos:          0,
+		expectedBits: []byte{0b00000001, 0b00000000},
+	},
+	{
+		name: "Set second bit in second byte",
+		bf: &BitField{
+			bits: []byte{0b00000000, 0b00000000},
+			sz:   16,
+		},
+		pos:          9,
+		expectedBits: []byte{0b00000000, 0b00000010},
+	},
+	{
+		name: "Set bit out of range",
+		bf: &BitField{
+			bits: []byte{0b10101010},
+			sz:   8,
+		},
+		pos:         100,
+		expectError: true,
+	},
+}
+
+var clearBitTestCases = []ClearBitTestCase{
+	{
+		name: "Clear first bit in 2-byte field",
+		bf: &BitField{
+			bits: []byte{0b11111111, 0b11111111},
+			sz:   16,
+		},
+		pos:          0,
+		expectedBits: []byte{0b11111110, 0b11111111},
+	},
+	{
+		name: "Clear second bit in second byte",
+		bf: &BitField{
+			bits: []byte{0b11111111, 0b11111111},
+			sz:   16,
+		},
+		pos:          9,
+		expectedBits: []byte{0b11111111, 0b11111101},
+	},
+	{
+		name: "Clear bit out of range",
+		bf: &BitField{
+			bits: []byte{0b10101010},
+			sz:   8,
+		},
+		pos:         100,
+		expectError: true,
+	},
+}
+
+var toggleBitTestCases = []ToggleBitTestCase{
+	{
+		name: "Toggle first bit in 2-byte field",
+		bf: &BitField{
+			bits: []byte{0b00000000, 0b11111111},
+			sz:   16,
+		},
+		pos:          0,
+		expectedBits: []byte{0b00000001, 0b11111111},
+	},
+	{
+		name: "Toggle second bit in second byte",
+		bf: &BitField{
+			bits: []byte{0b00000000, 0b11111111},
+			sz:   16,
+		},
+		pos:          9,
+		expectedBits: []byte{0b00000000, 0b11111101},
+	},
+	{
+		name: "Toggle bit out of range",
+		bf: &BitField{
+			bits: []byte{0b10101010},
+			sz:   8,
+		},
+		pos:         100,
+		expectError: true,
+	},
+}
+
+var testBitTestCases = []TestBitTestCase{
+	{
+		name: "Test first bit set in 2-byte field",
+		bf: &BitField{
+			bits: []byte{0b00000001, 0b00000000},
+			sz:   16,
+		},
+		pos:           0,
+		expectedValue: true,
+	},
+	{
+		name: "Test second bit not set in second byte",
+		bf: &BitField{
+			bits: []byte{0b10000000, 0b00000000},
+			sz:   16,
+		},
+		pos:           9,
+		expectedValue: false,
+	},
+	{
+		name: "Test bit out of range",
+		bf: &BitField{
+			bits: []byte{0b10101010},
+			sz:   8,
+		},
+		pos:         100,
+		expectError: true,
+	},
+}
+
 func TestNew(t *testing.T) {
 	for _, tc := range newTestCases {
 		t.Run(tc.name, func(t *testing.T) {
@@ -46,6 +197,81 @@ func TestNew(t *testing.T) {
 			}
 			if bf.sz != tc.n {
 				t.Errorf("%s: expected size %d, got %d", tc.name, tc.n, bf.sz)
+			}
+		})
+	}
+}
+
+func TestSetBit(t *testing.T) {
+	for _, tc := range setBitTestCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.bf.SetBit(tc.pos)
+
+			// Check for error consistency
+			if (err != nil) != tc.expectError {
+				t.Errorf("SetBit() error = %v, expectError %v", err, tc.expectError)
+				return
+			}
+
+			// Compare the resulting byte slice with the expected slice, if no error is expected
+			if !tc.expectError && !reflect.DeepEqual(tc.bf.bits, tc.expectedBits) {
+				t.Errorf("SetBit() got %v, want %v", tc.bf.bits, tc.expectedBits)
+			}
+		})
+	}
+}
+
+func TestClearBit(t *testing.T) {
+	for _, tc := range clearBitTestCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.bf.ClearBit(tc.pos)
+
+			// Check for error consistency
+			if (err != nil) != tc.expectError {
+				t.Errorf("ClearBit() error = %v, expectError %v", err, tc.expectError)
+				return
+			}
+
+			// Compare the resulting byte slice with the expected slice, if no error is expected
+			if !tc.expectError && !reflect.DeepEqual(tc.bf.bits, tc.expectedBits) {
+				t.Errorf("ClearBit() got %v, want %v", tc.bf.bits, tc.expectedBits)
+			}
+		})
+	}
+}
+
+func TestToggleBit(t *testing.T) {
+	for _, tc := range toggleBitTestCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.bf.ToggleBit(tc.pos)
+
+			// Check for error consistency
+			if (err != nil) != tc.expectError {
+				t.Errorf("ToggleBit() error = %v, expectError %v", err, tc.expectError)
+				return
+			}
+
+			// Compare the resulting byte slice with the expected slice, if no error is expected
+			if !tc.expectError && !reflect.DeepEqual(tc.bf.bits, tc.expectedBits) {
+				t.Errorf("ToggleBit() got %v, want %v", tc.bf.bits, tc.expectedBits)
+			}
+		})
+	}
+}
+
+func TestTestBit(t *testing.T) {
+	for _, tc := range testBitTestCases {
+		t.Run(tc.name, func(t *testing.T) {
+			value, err := tc.bf.TestBit(tc.pos)
+
+			if errOccurred := err != nil; tc.expectError != errOccurred {
+				if tc.expectError {
+					t.Errorf("%s: expected an error, but got none", tc.name)
+				} else {
+					t.Errorf("%s: did not expect an error, but got %v", tc.name, err)
+				}
+			} else if !tc.expectError && value != tc.expectedValue {
+				t.Errorf("%s: expected value %t, got %t", tc.name, tc.expectedValue, value)
 			}
 		})
 	}
